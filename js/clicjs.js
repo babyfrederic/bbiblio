12b062e801a31b00a6beb884ea18c066708be4d9
babyfrederic <62266289+babyfrederic@users.noreply.github.com>
2020-12-01 08:22:03 +0100

    mar

--- js/clicjs.js
+++ js/clicjs.js
@@ -4,48 +4,46 @@
     var statresult = 1 ;
     var total = 0 ;
 	function Count() {
-	    document.getElementById("output").innerHTML = i++ + 1;
+	    document.getElementById("input").innerHTML = i++ + 1;
 	}
 
 	function Count_1() {
-	    document.getElementById("output_1").innerHTML = j++ + 1;
+	    document.getElementById("output").innerHTML = j++ + 1;
 	}
 	function ecart(){
-	   var t = document.getElementById("output").innerText;
-	   var i = document.getElementById("output_1").innerText;
+	   var t = document.getElementById("input").innerText;
+	   var i = document.getElementById("output").innerText;
 	   document.getElementById("resultat").innerHTML = t - i;
 	}
 	function zero(){
-	    document.getElementById("output").innerHTML = 0;
-	    document.getElementById("output_1").innerHTML =0;
+	    document.getElementById("input").innerHTML = 0;
+	    document.getElementById("output").innerHTML =0;
 	    document.getElementById("resultat").innerHTML =0;
 	    i = 0;
 	    j = 0;
     }
      /**** partie javascript statistiques ****/ 
-    var stat_ent= function stat(stat_entrees){
-        var t = document.getElementById("output").innerText;
-        var i = document.getElementById("output_1").innerText;
-	    var stat_sorties = document.getElementById("output_1").innerText;
-        var ecart = document.getElementById("resultat").innerHTML = t - i;
-        var stat_total = document.getElementById("statotal").innerHTML = total++ + (stat_entrees - stat_sorties);
-        var stat_entrees = stat_entrees + t ;
-	    var stat_sorties = stat_sorties + i ;
-        var ecart = ecart + (t-i);
-        var stat_total = stat_entrees - stat_sorties;
-        return stat_entrees ;
+    function stat(){
+        var ii = document.getElementById("inputs").innerText;
+        var ss = document.getElementById("outputs").innerText;
+        var ecart = document.getElementById("resultats").innerHTML = ii - ss;
+        document.getElementById("statotal").innerHTML = total++ + (stat_entrees - stat_sorties);
+        var stat_entrees = stat_entrees + ii ;
+	    var stat_sorties = stat_sorties + ss ;
+        var ecart = ecart + (ii-ss);
+        statresult = statresult + 1;
     }
         function statplus(se,ss,ec,st){
-        var stat_entrees = stat_entrees++ + t ;
-	    var stat_sorties = stat_sorties++ + i ;
-        var ecart = ecart++ + (t-i);
+        var stat_entrees = stat_entrees++ + iii ;
+	    var stat_sorties = stat_sorties++ + sss ;
+        var ecart = ecart++ + (iii-sss);
         var stat_total = stat_entrees - stat_sorties;
         se = stat_entrees ;
         ss = stat_sorties ;
         ec = ecart ;
         st = stat_total ;
-        document.getElementById("periode").innerHTML = statresult++ + 1;
-        document.getElementById("statotal").innerHTML = total++ +(t-i); 
+        document.getElementById("periodes").innerHTML = statresult++ + 1;
+        document.getElementById("statotal").innerHTML = total++ +(iii-sss); 
         return (se,ss,ec,st);
     }
     function zerostat(){
@@ -55,11 +53,11 @@
         var stat_total =0;
         var statresult =1;
         var total =0;
-        document.getElementById("output").innerHTML = 0;
-	    document.getElementById("output_1").innerHTML =0;
-	    document.getElementById("resultat").innerHTML =0;
-        document.getElementById("stat").innerHTML =0;
-        document.getElementById("periode").innerHTML =0;
+        document.getElementById("inputs").innerHTML = 0;
+	    document.getElementById("outputs").innerHTML =0;
+	    document.getElementById("resultats").innerHTML =0;
+        document.getElementById("statotal").innerHTML =0;
+        document.getElementById("periodes").innerHTML =0;
         document.getElementById("statotal").innerHTML =0; 
     }
     
