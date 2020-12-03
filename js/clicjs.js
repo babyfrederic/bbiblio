f2259699980361bf8723469502f2f4e55c1ee5dc
babyfrederic <62266289+babyfrederic@users.noreply.github.com>
2020-12-03 21:21:30 +0100

    sum

--- js/clicjs.js
+++ js/clicjs.js
@@ -1,3 +1,25 @@
+/*** Horloge de param ***/
+window.addEventListener('load',horloge);
+      function horloge(){
+        let d = new Date();
+        document.getElementById('heure').innerHTML = d.toLocaleTimeString();
+        setTimeout(horloge, 1000);
+    }
+    
+      document.addEventListener('DOMContentLoaded', function(){
+        let cache = document.getElementById('bouton');
+        cache.addEventListener('click',cacheHorloge);
+        document.getElementById('tog').style.display = 'block';
+        function cacheHorloge(){
+          let para = document.getElementById('tog');
+          if(para.style.display == 'block'){
+            para.style.display = 'none';
+          }else{
+            para.style.display = 'block';
+          }
+        }
+      });
+
 /*** valeurs pour le seuil d'alerte ***/
 var seuil =0;
 var surface = 120;
